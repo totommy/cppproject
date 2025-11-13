a977b327f2f955b5e4e08c2e353edbcbc189f174
tommy <tommychen494@gmail.com>
2025-11-13 13:59:19 +0800

    final

--- include/ThreadPool.hpp
+++ include/ThreadPool.hpp
@@ -81,28 +81,5 @@ public:
 void task(int& i) {
     cout << "finished task " << i << endl;
 }
-// void test() {
-//     ThreadPool threadpool(5);
-//     // 建構子是直接建構 5 個多線程(這裡是消費者模式)來處理任務隊列中的任務
-//     // 並利用 cv 來讓他們睡覺 
-//     // 等到有任務在任務隊列中 才會被生產者喚起
-//     for (int i = 1; i <= 10; i++) {
-//         cout << "add task " << i << endl;
-//         // 一定要先輸出 add task 再加入任務到隊列
-//         // 不然一放任務 線程就會去完成任務而輸出 finished task
-//         threadpool.addTask(task, ref(i));
-//         // 這裡是生產者模式: 會加入任務到任務隊列中
-//         this_thread::sleep_for(std::chrono::milliseconds(1000));
-//         // 模擬延遲
-//     }
-//     // 函數結束後 物件會銷毀 就要讓多線程全部運行完成並且 join (寫在解構子中)
-// }
 
-
-// 生產者 - 消費者模型 : 無多線程版本
-// 生產者 - 消費者模型 : 多線程版本
-
-
-// 設計模式: 單例模式
-// 單例模式與多線程: 用 call_once 解決
 #endif
